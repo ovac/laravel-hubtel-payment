c2daee25c8500fe80a81ad4e0a81554640b5f53a
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-02-19 12:49:47 +0000

    Complete sensiolabs fixes

--- sami.config.php
+++ sami.config.php
@@ -1,10 +1,7 @@
 <?php
 
-// require_once 'vendor/autoload.php';
-
 use Sami\RemoteRepository\GitHubRemoteRepository;
 use Sami\Sami;
-// use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
 $finder = Finder::create()
@@ -12,13 +9,6 @@ $finder = Finder::create()
     ->name('*.php')
     ->in($dir = __DIR__ . '/src')
 ;
-
-// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
-// $versions = GitVersionCollection::create($dir)
-//     ->addFromTags('v1.0.*')
-// // ->add('1.0', '1.0 branch')
-//     ->add('master', 'master branch')
-// ;
 
 $config = array(
     'theme' => 'ovac',
